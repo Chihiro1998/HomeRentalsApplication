df0fd464534ed2348cf6b11721ad3a2a6375a592
olivia kuang <kuangyuchen0301@gmail.com>
2025-02-10 18:27:12 -0500

    Fix bug

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -3,15 +3,16 @@ import { categories } from "../data";
 import "../styles/Listings.scss";
 
 const Listings = () => {
-  return;
-  <div className="category-list">
-    {categories?.map((category, index) => {
-      <div className="category" key={index}>
-        <div className="category_icon">{category.icon}</div>
-        <p>{category.label}</p>
-      </div>;
-    })}
-  </div>;
+  return (
+    <div className="category-list">
+      {categories?.map((category, index) => (
+        <div className="category" key={index}>
+          <div className="category_icon">{category.icon}</div>
+          <p>{category.label}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Listings;
